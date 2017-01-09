c33cb29f99c2ea49d21c7b6eac23eddc3d174841
Harrison Wong <harrisonwong@Harrisons-MacBook-Pro.local>
2017-01-09 11:16:43 -0500

    Initial Commit

---
+++ Tic-Tac-Toe2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe2
+//
+//  Created by Harrison Wong on 1/09/2017.
+//  Copyright © 2017 Harrison Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
